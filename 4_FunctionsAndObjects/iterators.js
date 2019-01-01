2a5b93d5d05f1cb7c651f25bfaf360c27e6e8468
Ife <ife.jibola@gmail.com>
2019-01-01 17:59:35 -0500

    customizing iterators

---
+++ 4_FunctionsAndObjects/iterators.js
@@ -0,0 +1,38 @@
+const iterators = ()=>{
+
+    var title = 'ES6';
+    console.log(typeof title [Symbol.iterator]);
+
+    var iterateIt = title[Symbol.iterator]();
+    console.log(iterateIt.next());
+    console.log(iterateIt.next());
+    console.log(iterateIt.next());
+    console.log(iterateIt.next());
+
+    function tableReady(arr){
+        var nextIndex = 0;
+        return{
+            next(){
+                if(nextIndex < arr.length){
+                    return{ value: arr.shift(), done: false}
+                }else{
+                    return {done: true}
+                }
+            }
+        }
+    }
+
+    var waitingList = ['Sarah', 'Heather', 'Anna', 'Meagan'];
+    var iterateList = tableReady(waitingList);
+
+    console.log(`${iterateList.next().value}, your table is ready`);
+    console.log(`${iterateList.next().value}, your table is ready`);
+    console.log(`${iterateList.next().value}, your table is ready`);
+    console.log(`${iterateList.next().value}, your table is ready`);
+    console.log(`${iterateList.next().done}, your table is ready`);
+
+
+
+}
+
+export default iterators;
